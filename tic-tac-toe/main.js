ffb95a7073e7b64a9a31b19a3a085343e210ec12
Ambarish Chatterjee <ambarish@ewaycorp.com>
2019-03-11 01:53:42 +0530

    Homework Assignment #8

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,7 +1,23 @@
 //gamepad area
 const gameBoard = document.getElementById("gameBoard");
 
+function createBoard(){
+    for (let i = 0; i < 9; i++) {
+        console.log(i);
+        
+        const gamecell = document.createElement("div");
+        gamecell.className("gamecell");
+        
+    }
+    
+};
+createBoard();
+
 const ticBox = gameBoard.querySelectorAll(".tic");
+
+const winningComb = [
+    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+];
 
 ticBox.forEach((tac,index) => {
     tac.innerHTML = "";
